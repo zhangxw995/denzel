f0fcad4657b8ff824fe156d16c3d69ae033ebabb
xuewen zhang <zxw985795572@qq.com>
2019-03-23 06:29:16 +0100

    project final version

--- REST-API/rest.js
+++ REST-API/rest.js
@@ -3,19 +3,16 @@ let bodyParser = require('body-parser');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://zxw:zxw@zxwcluster-9i4xf.mongodb.net/test?retryWrites=true"
-// Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-
 app.get('/movies/populate', function (req, res) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     var dbo = db.db("zxw");
-    dbo.collection("movies"). find({}).toArray(function(err, result) { // 返回集合中所有数据
+    dbo.collection("movies"). find({}).toArray(function(err, result) {
         len = result.length
-        console.log(len)
         var countt = {"count":len}
         res.send(countt);
         db.close();
@@ -27,8 +24,7 @@ app.get('/movies/populate', function (req, res) {
 app.get('/movies', function (req, res) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     var dbo = db.db("zxw");
-    dbo.collection("movies"). find({}).toArray(function(err, result) { // 返回集合中所有数据
-        console.log(result);
+    dbo.collection("movies"). find({}).toArray(function(err, result) { 
         var round_num = Math.round(Math.random()); 
         res.send(result[round_num]);
         db.close();
@@ -39,10 +35,8 @@ app.get('/movies', function (req, res) {
 app.get('/movie/:id', function (req, res) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     var dbo = db.db("zxw");
-    console.log(req.params.id)
     var whereStr = {"id":req.params.id}; 
-    dbo.collection("movies"). find(whereStr).toArray(function(err, result) { // 返回集合中所有数据
-        console.log(result);
+    dbo.collection("movies"). find(whereStr).toArray(function(err, result) { 
         res.send(result);
         db.close();
     });
@@ -52,14 +46,12 @@ app.get('/movie/:id', function (req, res) {
  
 
  app.get('^/movies/search', function (req, res) {
-   console.log(req.query.limit);
-   console.log(req.query.metascore);
    var score = parseInt(req.query.metascore)
    var count = parseInt(req.query.limit)
    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
     var dbo = db.db("zxw");
     var whereStr = {"metascore":{$gt: score}}; 
-    dbo.collection("movies"). find(whereStr).limit(count).toArray(function(err, result) { // 返回集合中所有数据
+    dbo.collection("movies"). find(whereStr).limit(count).toArray(function(err, result) { 
         console.log(result);
         console.log(typeof result)
         res.send(result);
@@ -73,8 +65,7 @@ app.get('/movie/:id', function (req, res) {
     var dbo = db.db("zxw");
     var whereStr = {"id":req.params.id}; 
     var updateStr = {$set: { "date" : req.body.date,"review":req.body.review}};
-    dbo.collection("movies").updateOne(whereStr, updateStr,function(err, result) { // 返回集合中所有数据
-        console.log(result);
+    dbo.collection("movies").updateOne(whereStr, updateStr,function(err, result) { 
         res.send(result);
         db.close();
     });
